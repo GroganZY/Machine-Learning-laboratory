79e2a7942eedde5266abf73b4730c15eef194e77
DESKTOP-C35Q1V2\11853 <grogan_zy@163.com>
2022-12-20 22:51:06 +0800

    实验四

--- 实验四/1.调包支持向量机分类鸢尾花/实验4_1.py
+++ 实验四/1.调包支持向量机分类鸢尾花/实验4_1.py
@@ -4,7 +4,7 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import KFold
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
-
+from sklearn.svm import SVC
 # data
 def create_data():
     iris = load_iris()
@@ -20,12 +20,29 @@ def create_data():
 
 X, y = create_data()
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
-
+iris = load_iris()
+# 可视化数据集
+plt.scatter(iris.data[:, 0], iris.data[:, 1], c=iris.target)
+plt.xlabel(iris.feature_names[0])
+plt.ylabel(iris.feature_names[1])
+plt.show()
 
 """数据处理完成"""
 
-from sklearn.svm import SVC
-clf = SVC()
+
+
+clf = SVC(kernel='linear')
 clf.fit(X_train, y_train)
 
-print(clf.score(X_test, y_test))
+accuracy = clf.score(X_test, y_test)
+print("Accuracy: ", accuracy)
+
+
+# 使用训练好的模型对测试集进行预测
+y_pred = clf.predict(X_test)
+
+# 可视化预测结果
+plt.scatter(X_test[:, 0], X_test[:, 1], c=y_pred)
+plt.xlabel(iris.feature_names[0])
+plt.ylabel(iris.feature_names[1])
+plt.show()
